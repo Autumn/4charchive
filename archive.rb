c1bffd20ebdf329d3284435b2ada761d218891c5
Autumn <andrewpcoleman@gmail.com>
2014-04-22 03:02:22 +0000

    first

---
+++ archive.rb
@@ -0,0 +1,22 @@
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open(ARGV[0]))
+
+id = doc.css("div .thread").attr("id").value
+
+Dir.mkdir id unless File.exists? id
+Dir.chdir id
+
+posts = doc.css("div .postContainer")
+files = []
+posts.each do |post|
+  a = post.css("div .file a")[0]
+  if a != nil
+    files.push a.attr("href")
+  end 
+end
+
+files.each do |file|
+  system "wget #{file.sub(/^\/\//, "")}" 
+end
